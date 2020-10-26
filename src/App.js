e686f98b767885d5cd4939e54030edd24bcddf51
vrshgv <170103104@stu.sdu.edu.kz>
2020-10-26 22:18:19 +0600

    app.js update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Footer from './components/Footer'
 import NavBar from './components/NavBar'
